80b7fd8b59a944cfabd03020be2cdc4515cbd0b5
Khthonian <anansicorp@tuta.io>
2023-08-15 11:48:22 +0100

    Add input splitting function

--- diceroll.lua
+++ diceroll.lua
@@ -6,3 +6,11 @@ function rollDice(faces)
     return math.random(1, faces)
 end
 
+-- Define a function to split the user input
+function splitInput(input, delimiter)
+    local output = {}
+    for match in (input..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(output, match)
+    end
+    return output
+end
